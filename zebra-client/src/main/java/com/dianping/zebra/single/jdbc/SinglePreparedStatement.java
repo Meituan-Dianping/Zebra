f9ee8b14f4504dc9d9f0adc4a09d853d1a359c2e
tong.xin <tong.xin@dianping.com>
2019-03-05 12:12:49 +0800

    fix params ordering in pstmt in another way, fixes #29

--- zebra-client/src/main/java/com/dianping/zebra/single/jdbc/SinglePreparedStatement.java
+++ zebra-client/src/main/java/com/dianping/zebra/single/jdbc/SinglePreparedStatement.java
@@ -74,6 +74,7 @@ import com.dianping.zebra.group.jdbc.param.TimestampParamContext;
 import com.dianping.zebra.group.jdbc.param.URLParamContext;
 import com.dianping.zebra.util.SqlType;
 import com.dianping.zebra.util.SqlUtils;
+import javafx.collections.transformation.SortedList;
 
 /**
  * @author hao.zhu
@@ -82,9 +83,16 @@ public class SinglePreparedStatement extends SingleStatement implements Prepared
 
 	private String sql;
 
-	private List<ParamContext> params = new ArrayList<ParamContext>();
+	private List<ParamContext> params = new LinkedList<>();
 
 	private List<List<ParamContext>> pstBatchedArgs;
+
+	private static Comparator PARAM_COMPARATOR = new Comparator<ParamContext>(){
+		@Override
+		public int compare(ParamContext paramContext1, ParamContext paramContext2) {
+			return paramContext1.getIndex() - paramContext2.getIndex();
+		}
+	};
 
 	public SinglePreparedStatement(String dsId, SingleConnection innerConnection, List<JdbcFilter> filters,
 			Statement stmt, String sql) throws SQLException {
@@ -121,6 +129,7 @@ public class SinglePreparedStatement extends SingleStatement implements Prepared
 
 	@Override
 	public ResultSet executeQuery() throws SQLException {
+		Collections.sort(params, PARAM_COMPARATOR);
 		return executeWithFilter(new JdbcOperationCallback<ResultSet>() {
 			@Override
 			public ResultSet doAction(Connection conn) throws SQLException {
@@ -132,6 +141,7 @@ public class SinglePreparedStatement extends SingleStatement implements Prepared
 
 	@Override
 	public int executeUpdate() throws SQLException {
+		Collections.sort(params, PARAM_COMPARATOR);
 		return executeWithFilter(new JdbcOperationCallback<Integer>() {
 			@Override
 			public Integer doAction(Connection conn) throws SQLException {
@@ -176,8 +186,9 @@ public class SinglePreparedStatement extends SingleStatement implements Prepared
 		if (pstBatchedArgs == null) {
 			pstBatchedArgs = new ArrayList<List<ParamContext>>();
 		}
-		List<ParamContext> newArgs = new ArrayList<ParamContext>(params.size());
-		newArgs.addAll(params);
+
+		List<ParamContext> newArgs = new ArrayList<ParamContext>(params);
+		Collections.sort(newArgs, PARAM_COMPARATOR);
 
 		params.clear();
 
@@ -193,290 +204,290 @@ public class SinglePreparedStatement extends SingleStatement implements Prepared
 
 	@Override
 	public void setNull(int parameterIndex, int sqlType) throws SQLException {
-		params.add(parameterIndex - 1, new NullParamContext(parameterIndex, new Object[] { sqlType }));
+		params.add(new NullParamContext(parameterIndex, new Object[] { sqlType }));
 		getPreparedStatement().setNull(parameterIndex, sqlType);
 	}
 
 	@Override
 	public void setBoolean(int parameterIndex, boolean x) throws SQLException {
-		params.add(parameterIndex - 1, new BooleanParamContext(parameterIndex, new Object[] { x }));
+		params.add(new BooleanParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setBoolean(parameterIndex, x);
 	}
 
 	@Override
 	public void setByte(int parameterIndex, byte x) throws SQLException {
-		params.add(parameterIndex - 1, new ByteParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ByteParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setByte(parameterIndex, x);
 	}
 
 	@Override
 	public void setShort(int parameterIndex, short x) throws SQLException {
-		params.add(parameterIndex - 1, new ShortParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ShortParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setShort(parameterIndex, x);
 	}
 
 	@Override
 	public void setInt(int parameterIndex, int x) throws SQLException {
-		params.add(parameterIndex - 1, new IntParamContext(parameterIndex, new Object[] { x }));
+		params.add(new IntParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setInt(parameterIndex, x);
 	}
 
 	@Override
 	public void setLong(int parameterIndex, long x) throws SQLException {
-		params.add(parameterIndex - 1, new LongParamContext(parameterIndex, new Object[] { x }));
+		params.add(new LongParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setLong(parameterIndex, x);
 	}
 
 	@Override
 	public void setFloat(int parameterIndex, float x) throws SQLException {
-		params.add(parameterIndex - 1, new FloatParamContext(parameterIndex, new Object[] { x }));
+		params.add(new FloatParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setFloat(parameterIndex, x);
 	}
 
 	@Override
 	public void setDouble(int parameterIndex, double x) throws SQLException {
-		params.add(parameterIndex - 1, new DoubleParamContext(parameterIndex, new Object[] { x }));
+		params.add(new DoubleParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setDouble(parameterIndex, x);
 	}
 
 	@Override
 	public void setBigDecimal(int parameterIndex, BigDecimal x) throws SQLException {
-		params.add(parameterIndex - 1, new BigDecimalParamContext(parameterIndex, new Object[] { x }));
+		params.add(new BigDecimalParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setBigDecimal(parameterIndex, x);
 	}
 
 	@Override
 	public void setString(int parameterIndex, String x) throws SQLException {
-		params.add(parameterIndex - 1, new StringParamContext(parameterIndex, new Object[] { x }));
+		params.add(new StringParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setString(parameterIndex, x);
 	}
 
 	@Override
 	public void setBytes(int parameterIndex, byte[] x) throws SQLException {
-		params.add(parameterIndex - 1, new ByteArrayParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ByteArrayParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setBytes(parameterIndex, x);
 	}
 
 	@Override
 	public void setDate(int parameterIndex, Date x) throws SQLException {
-		params.add(parameterIndex - 1, new DateParamContext(parameterIndex, new Object[] { x }));
+		params.add(new DateParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setDate(parameterIndex, x);
 	}
 
 	@Override
 	public void setTime(int parameterIndex, Time x) throws SQLException {
-		params.add(parameterIndex - 1, new TimeParamContext(parameterIndex, new Object[] { x }));
+		params.add(new TimeParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setTime(parameterIndex, x);
 	}
 
 	@Override
 	public void setTimestamp(int parameterIndex, Timestamp x) throws SQLException {
-		params.add(parameterIndex - 1, new TimestampParamContext(parameterIndex, new Object[] { x }));
+		params.add(new TimestampParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setTimestamp(parameterIndex, x);
 	}
 
 	@Override
 	public void setAsciiStream(int parameterIndex, InputStream x, int length) throws SQLException {
-		params.add(parameterIndex - 1, new AsciiParamContext(parameterIndex, new Object[] { x, length }));
+		params.add(new AsciiParamContext(parameterIndex, new Object[] { x, length }));
 		getPreparedStatement().setAsciiStream(parameterIndex, x, length);
 	}
 
 	@Override
 	@Deprecated
 	public void setUnicodeStream(int parameterIndex, InputStream x, int length) throws SQLException {
-		params.add(parameterIndex - 1, new AsciiParamContext(parameterIndex, new Object[] { x }));
+		params.add(new AsciiParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setUnicodeStream(parameterIndex, x, length);
 	}
 
 	@Override
 	public void setBinaryStream(int parameterIndex, InputStream x, int length) throws SQLException {
-		params.add(parameterIndex - 1, new BinaryStreamParamContext(parameterIndex, new Object[] { x, length }));
+		params.add(new BinaryStreamParamContext(parameterIndex, new Object[] { x, length }));
 		getPreparedStatement().setBinaryStream(parameterIndex, x, length);
 	}
 
 	@Override
 	public void setObject(int parameterIndex, Object x, int targetSqlType) throws SQLException {
-		params.add(parameterIndex - 1, new ObjectParamContext(parameterIndex, new Object[] { x, targetSqlType }));
+		params.add(new ObjectParamContext(parameterIndex, new Object[] { x, targetSqlType }));
 		getPreparedStatement().setObject(parameterIndex, x, targetSqlType);
 	}
 
 	@Override
 	public void setObject(int parameterIndex, Object x) throws SQLException {
-		params.add(parameterIndex - 1, new ObjectParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ObjectParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setObject(parameterIndex, x);
 	}
 
 	@Override
 	public void setCharacterStream(int parameterIndex, Reader reader, int length) throws SQLException {
-		params.add(parameterIndex - 1, new CharacterStreamParamContext(parameterIndex, new Object[] { reader, length }));
+		params.add(new CharacterStreamParamContext(parameterIndex, new Object[] { reader, length }));
 		getPreparedStatement().setCharacterStream(parameterIndex, reader, length);
 	}
 
 	@Override
 	public void setRef(int parameterIndex, Ref x) throws SQLException {
-		params.add(parameterIndex - 1, new RefParamContext(parameterIndex, new Object[] { x }));
+		params.add(new RefParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setRef(parameterIndex, x);
 	}
 
 	@Override
 	public void setBlob(int parameterIndex, Blob x) throws SQLException {
-		params.add(parameterIndex - 1, new BlobParamContext(parameterIndex, new Object[] { x }));
+		params.add(new BlobParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setBlob(parameterIndex, x);
 	}
 
 	@Override
 	public void setClob(int parameterIndex, Clob x) throws SQLException {
-		params.add(parameterIndex - 1, new ClobParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ClobParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setClob(parameterIndex, x);
 	}
 
 	@Override
 	public void setArray(int parameterIndex, Array x) throws SQLException {
-		params.add(parameterIndex - 1, new ArrayParamContext(parameterIndex, new Object[] { x }));
+		params.add(new ArrayParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setArray(parameterIndex, x);
 	}
 
 	@Override
 	public void setDate(int parameterIndex, Date x, Calendar cal) throws SQLException {
-		params.add(parameterIndex - 1, new DateParamContext(parameterIndex, new Object[] { x, cal }));
+		params.add(new DateParamContext(parameterIndex, new Object[] { x, cal }));
 		getPreparedStatement().setDate(parameterIndex, x, cal);
 	}
 
 	@Override
 	public void setTime(int parameterIndex, Time x, Calendar cal) throws SQLException {
-		params.add(parameterIndex - 1, new TimeParamContext(parameterIndex, new Object[] { x, cal }));
+		params.add(new TimeParamContext(parameterIndex, new Object[] { x, cal }));
 		getPreparedStatement().setTime(parameterIndex, x, cal);
 	}
 
 	@Override
 	public void setTimestamp(int parameterIndex, Timestamp x, Calendar cal) throws SQLException {
-		params.add(parameterIndex - 1, new TimestampParamContext(parameterIndex, new Object[] { x, cal }));
+		params.add(new TimestampParamContext(parameterIndex, new Object[] { x, cal }));
 		getPreparedStatement().setTimestamp(parameterIndex, x, cal);
 	}
 
 	@Override
 	public void setNull(int parameterIndex, int sqlType, String typeName) throws SQLException {
-		params.add(parameterIndex - 1, new NullParamContext(parameterIndex, new Object[] { sqlType, typeName }));
+		params.add(new NullParamContext(parameterIndex, new Object[] { sqlType, typeName }));
 		getPreparedStatement().setNull(parameterIndex, sqlType, typeName);
 	}
 
 	@Override
 	public void setURL(int parameterIndex, URL x) throws SQLException {
-		params.add(parameterIndex - 1, new URLParamContext(parameterIndex, new Object[] { x }));
+		params.add(new URLParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setURL(parameterIndex, x);
 	}
 
 	@Override
 	public void setRowId(int parameterIndex, RowId x) throws SQLException {
-		params.add(parameterIndex - 1, new RowIdParamContext(parameterIndex, new Object[] { x }));
+		params.add(new RowIdParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setRowId(parameterIndex, x);
 	}
 
 	@Override
 	public void setNString(int parameterIndex, String value) throws SQLException {
-		params.add(parameterIndex - 1, new NStringParamContext(parameterIndex, new Object[] { value }));
+		params.add(new NStringParamContext(parameterIndex, new Object[] { value }));
 		getPreparedStatement().setNString(parameterIndex, value);
 	}
 
 	@Override
 	public void setNCharacterStream(int parameterIndex, Reader value, long length) throws SQLException {
-		params.add(parameterIndex - 1, new NCharacterStreamParamContext(parameterIndex, new Object[] { value, length }));
+		params.add(new NCharacterStreamParamContext(parameterIndex, new Object[] { value, length }));
 		getPreparedStatement().setNCharacterStream(parameterIndex, value, length);
 	}
 
 	@Override
 	public void setNClob(int parameterIndex, NClob value) throws SQLException {
-		params.add(parameterIndex - 1, new NClobParamContext(parameterIndex, new Object[] { value }));
+		params.add(new NClobParamContext(parameterIndex, new Object[] { value }));
 		getPreparedStatement().setNClob(parameterIndex, value);
 	}
 
 	@Override
 	public void setClob(int parameterIndex, Reader reader, long length) throws SQLException {
-		params.add(parameterIndex - 1, new ClobParamContext(parameterIndex, new Object[] { reader, length }));
+		params.add(new ClobParamContext(parameterIndex, new Object[] { reader, length }));
 		getPreparedStatement().setClob(parameterIndex, reader, length);
 	}
 
 	@Override
 	public void setBlob(int parameterIndex, InputStream inputStream, long length) throws SQLException {
-		params.add(parameterIndex - 1, new BlobParamContext(parameterIndex, new Object[] { inputStream, length }));
+		params.add(new BlobParamContext(parameterIndex, new Object[] { inputStream, length }));
 		getPreparedStatement().setBlob(parameterIndex, inputStream, length);
 	}
 
 	@Override
 	public void setNClob(int parameterIndex, Reader reader, long length) throws SQLException {
-		params.add(parameterIndex - 1, new NClobParamContext(parameterIndex, new Object[] { reader, length }));
+		params.add(new NClobParamContext(parameterIndex, new Object[] { reader, length }));
 		getPreparedStatement().setNClob(parameterIndex, reader, length);
 	}
 
 	@Override
 	public void setSQLXML(int parameterIndex, SQLXML xmlObject) throws SQLException {
-		params.add(parameterIndex - 1, new SQLXMLParamContext(parameterIndex, new Object[] { xmlObject }));
+		params.add(new SQLXMLParamContext(parameterIndex, new Object[] { xmlObject }));
 		getPreparedStatement().setSQLXML(parameterIndex, xmlObject);
 	}
 
 	@Override
 	public void setObject(int parameterIndex, Object x, int targetSqlType, int scaleOrLength) throws SQLException {
-		params.add(parameterIndex - 1, new ObjectParamContext(parameterIndex, new Object[] { x, targetSqlType, scaleOrLength }));
+		params.add(new ObjectParamContext(parameterIndex, new Object[] { x, targetSqlType, scaleOrLength }));
 		getPreparedStatement().setObject(parameterIndex, x, targetSqlType, scaleOrLength);
 	}
 
 	@Override
 	public void setAsciiStream(int parameterIndex, InputStream x, long length) throws SQLException {
-		params.add(parameterIndex - 1, new AsciiParamContext(parameterIndex, new Object[] { x, length }));
+		params.add(new AsciiParamContext(parameterIndex, new Object[] { x, length }));
 		getPreparedStatement().setAsciiStream(parameterIndex, x, length);
 	}
 
 	@Override
 	public void setBinaryStream(int parameterIndex, InputStream x, long length) throws SQLException {
-		params.add(parameterIndex - 1, new BinaryStreamParamContext(parameterIndex, new Object[] { x, length }));
+		params.add(new BinaryStreamParamContext(parameterIndex, new Object[] { x, length }));
 		getPreparedStatement().setBinaryStream(parameterIndex, x, length);
 	}
 
 	@Override
 	public void setCharacterStream(int parameterIndex, Reader reader, long length) throws SQLException {
-		params.add(parameterIndex - 1, new CharacterStreamParamContext(parameterIndex, new Object[] { reader, length }));
+		params.add(new CharacterStreamParamContext(parameterIndex, new Object[] { reader, length }));
 		getPreparedStatement().setCharacterStream(parameterIndex, reader, length);
 	}
 
 	@Override
 	public void setAsciiStream(int parameterIndex, InputStream x) throws SQLException {
-		params.add(parameterIndex - 1, new AsciiParamContext(parameterIndex, new Object[] { x }));
+		params.add(new AsciiParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setAsciiStream(parameterIndex, x);
 	}
 
 	@Override
 	public void setBinaryStream(int parameterIndex, InputStream x) throws SQLException {
-		params.add(parameterIndex - 1, new BinaryStreamParamContext(parameterIndex, new Object[] { x }));
+		params.add(new BinaryStreamParamContext(parameterIndex, new Object[] { x }));
 		getPreparedStatement().setBinaryStream(parameterIndex, x);
 	}
 
 	@Override
 	public void setCharacterStream(int parameterIndex, Reader reader) throws SQLException {
-		params.add(parameterIndex - 1, new CharacterStreamParamContext(parameterIndex, new Object[] { reader }));
+		params.add(new CharacterStreamParamContext(parameterIndex, new Object[] { reader }));
 		getPreparedStatement().setCharacterStream(parameterIndex, reader);
 	}
 
 	@Override
 	public void setNCharacterStream(int parameterIndex, Reader value) throws SQLException {
-		params.add(parameterIndex - 1, new NCharacterStreamParamContext(parameterIndex, new Object[] { value }));
+		params.add(new NCharacterStreamParamContext(parameterIndex, new Object[] { value }));
 		getPreparedStatement().setNCharacterStream(parameterIndex, value);
 	}
 
 	@Override
 	public void setClob(int parameterIndex, Reader reader) throws SQLException {
-		params.add(parameterIndex - 1, new ClobParamContext(parameterIndex, new Object[] { reader }));
+		params.add(new ClobParamContext(parameterIndex, new Object[] { reader }));
 		getPreparedStatement().setClob(parameterIndex, reader);
 	}
 
 	@Override
 	public void setBlob(int parameterIndex, InputStream inputStream) throws SQLException {
-		params.add(parameterIndex - 1, new BlobParamContext(parameterIndex, new Object[] { inputStream }));
+		params.add(new BlobParamContext(parameterIndex, new Object[] { inputStream }));
 		getPreparedStatement().setBlob(parameterIndex, inputStream);
 	}
 
 	@Override
 	public void setNClob(int parameterIndex, Reader reader) throws SQLException {
-		params.add(parameterIndex - 1, new NClobParamContext(parameterIndex, new Object[] { reader }));
+		params.add(new NClobParamContext(parameterIndex, new Object[] { reader }));
 		getPreparedStatement().setNCharacterStream(parameterIndex, reader);
 	}
 }
